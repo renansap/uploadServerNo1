aa84f85718d2ea3634f000a3071987475a49df87
Renan <>
2019-12-04 19:06:27 -0200

    Merge branch 'master' of https://github.com/renansap/uploadJava
    # Conflicts:
    #	Upload/src/uploads/TelaUp.java

--- src/serverno1/ServerNo1.java
+++ src/serverno1/ServerNo1.java
@@ -5,11 +5,18 @@
  */
 package serverno1;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -20,6 +27,7 @@ import java.util.Scanner;
  * @author igorv
  */
 public class ServerNo1 {
+    private static Object ToServer;
 
     /**
      * @param args the command line arguments
@@ -28,7 +36,8 @@ public class ServerNo1 {
 
         ServerSocket serverSocket = null;
         
-        String caminho = "C:\\Users\\Ksa\\Documents\\NetBeansProjects\\uploadServerNo1\\src\\arquivos\\";
+        //String caminho = "C:\\Users\\Ksa\\Documents\\NetBeansProjects\\uploadServerNo1\\src\\arquivos\\";
+        String caminho = "C:\\Users\\igor.amaral\\Documents\\NetBeansProjects\\arquivos\\";
 
         try {
             serverSocket = new ServerSocket(4001);
@@ -57,29 +66,74 @@ public class ServerNo1 {
             System.out.println("Não é possível obter o fluxo de entrada do soquete. ");
         }
 
-        try {
-            //System.out.println("Gettt "+in.());
-            out = new FileOutputStream(caminho + "novo1.txt");
-            //System.out.println("out "+ out.getClass().getName());
-            
-            
-           //System.out.println("Arquivo Copiando SeverNo1");
         
+        try {
+            int bytesAmount = 0;
+            byte[] bytes = new byte[1024 * 1024];
+            //BufferedReader InFromClient = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+            //DataOutputStream OutToClient = new DataOutputStream(socket.getOutputStream());
+            
+            final DataInputStream InFromClient = new DataInputStream(socket.getInputStream());
+            final DataOutputStream OutToClient = new DataOutputStream(socket.getOutputStream());            
+            int a = InFromClient.read();
+            if (a==0) {
+                System.out.println(InFromClient.read());
+                File env = new File(caminho + "novo.001");
+                System.out.println(env.getPath());
+                
+                FileInputStream fis = new FileInputStream(env);
+                BufferedInputStream bis = new BufferedInputStream(fis);
+                
+                OutputStream out1 = socket.getOutputStream();
+                while ((bytesAmount = bis.read(bytes)) > 0) {
+                    out1.write(bytes, 0, bytesAmount);
+                    //OutToClient.write(bytes, 0, bytesAmount);
+                }
+                System.out.println("envio do arquivo 001 para o cliente");
+            } else {
+                out = new FileOutputStream(caminho + "novo.001");
+                           //System.out.println("out "+ out.getClass().getName());
 
-        byte[] bytes = new byte[16*1024];
+                String caminhoServer = (caminho + "novo.001");
 
-        int count;
-        while ((count = in.read(bytes)) > 0) {
-            out.write(bytes, 0, count);
+                File busca = new File(caminhoServer);
+
+                FileInputStream fis1 = new FileInputStream(busca);
+                BufferedInputStream bis1 = new BufferedInputStream(fis1);
+
+               //        try (   FileInputStream fis = new FileInputStream(busca);
+                //                BufferedInputStream bis = new BufferedInputStream(fis)) {
+                //            
+                //        }
+                //        byte[] arq1 = busca;;
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                //byte[] bytes = new byte[16 * 1024];
+
+                int count;
+                while ((count = in.read(bytes)) > 0) {
+                    out.write(bytes, 0, count);
+                }
+
+                out.close();
+                in.close();
+                socket.close();
+                serverSocket.close();               
+            }
+
+
+            
+        } catch (IOException ex) {
+            System.out.println(ex);
         }
-
-        out.close();
-        in.close();
-        socket.close();
-        serverSocket.close();
-        } catch (FileNotFoundException ex) {
-            System.out.println("Arquivo não encontrado. ");
-        }
+        
+//        try {
+//            //System.out.println("Gettt "+in.());
+//            System.out.println("asd");
+//       
+//        } catch (FileNotFoundException ex) {
+//            System.out.println("Arquivo não encontrado. ");
+//        }
     }
 }
 
